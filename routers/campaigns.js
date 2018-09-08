9ccf2cc312c37e1be14c12c9cc08536c041c4412
Unknown <solipod8@gmail.com>
2018-09-08 17:06:00 +0300

    campaign api

---
+++ routers/campaigns.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const DAL = require('../DAL');
+const bodyParser = require('body-parser');
+const auth = require('../middlewares/authenticator');
+const router = express.Router();
+const CAMPAIGN_COL = 'campaigns';
+
+router.use(bodyParser.json());
+router.use('*', auth);
+
+// ################### API ################### //
+
+router.get('/', (req, res) => {
+    DAL.GetAll(CAMPAIGN_COL, (data) => {
+        res.send(data);
+    });
+});
+
+router.get('/:name', (req, res) => {
+    DAL.Get(CAMPAIGN_COL, {name: req.params.name}, (data) => {
+        res.send(data);
+    });
+});
+
+router.post('/', (req, res) => {
+    DAL.Insert(CAMPAIGN_COL, req.body, (data) => {
+        res.send(data);
+    });
+});
+
+router.put('/:name', (req, res) => {
+    DAL.Update(CAMPAIGN_COL, {name: req.params.name}, {$set: req.body}, (data) => {
+        res.send(data);
+   });
+});
+
+router.delete('/:name', (req, res) => {
+    DAL.Delete(CAMPAIGN_COL, {name: req.params.name}, (data) => {
+        res.send(data);
+    });
+});
+
+module.exports = [router];
