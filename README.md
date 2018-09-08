7c0e3f72f28bd94be1416272751e57d7a32950a7
Unknown <solipod8@gmail.com>
2018-09-08 17:03:54 +0300

    init project

--- README.md
+++
@@ -1,2 +0,0 @@
-# chab-ad
-ad manager
---
+++ server.js
@@ -0,0 +1,35 @@
+// ############################################## //
+// ################# - ChabAd - ################# //
+// ############################################## //
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.use(express.static(__dirname + '/public'));
+
+// ################ ROUTERS ################# //
+
+let campaigns = require('./routers/campaigns');
+let cloudinary = require('./routers/cloudinary');
+let ads = require('./routers/ads');
+
+// ################## API ################### //
+
+app.use('/campaigns', campaigns);
+app.use('/cloudinary', cloudinary);
+app.use('/ads', ads);
+
+app.get('/', (req, res) => {
+    res.sendFile('./index.html');
+});
+
+// ############# Start Server ############### //
+
+app.listen(port, (err) => {
+    if (err){
+        console.log(err);
+    } else {
+        console.log('Server is up, Port: ' + port);
+    }
+});
