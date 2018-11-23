b870f677684b7d02045565ecb0cce64f511d7d81
סול אילן <solipod8@gmail.com>
2018-11-23 04:59:13 +0200

    check starting date. amd refactor

--- BL.js
+++ BL.js
@@ -1,3 +1,4 @@
+const moment = require('moment');
 const DAL = require('./DAL');
 const schedule = require('node-schedule');
 const logger = require('./Logger');
@@ -5,158 +6,126 @@ const CAMPAIGN_COL = 'campaigns';
 const ADS_COL = 'ads';
 
 module.exports = {
-  ActiveCampaignScheuduler: () => {
-    try {
-      schedule.scheduleJob('0 0 * * *', function () {
-        deactivateCampaign({ "expiration_date": {$lte: new Date()}}, () => {
-          validateResources();
-        });
-      });  
-    } catch (err) {
-      handleErrors('CampaignSqudualer', err, () =>{
-        console.log("CampaignSqudualer falied");
-      });
-    }
-    
-  },
-    GetAds: (adsReqArr, callback) => {
-      let ads = [];
-
-      try {
-    
-        for (let i = 0; i < adsReqArr.length; i++) {
-          ads.push(getAd(adsReqArr[i]));
+    ActiveCampaignScheuduler: () => {
+        try {
+            let now = moment().format();
+            schedule.scheduleJob('0 0 * * *', function () {
+                validateCampaign({ "starting_date": { $lte: now }, "expiration_date": { $gte: now } }, true);
+                validateCampaign({ "expiration_date": { $lte: now } }, false);
+            });
+        } catch (err) {
+            handleErrors('CampaignSqudualer', err, () => {
+                console.log("CampaignSqudualer falied");
+            });
         }
-    
-        Promise.all(ads).then((data) => {
-          callback(data);
-          decrementViews(data);
-        });
-      
-      } catch (err) {
-        handleErrors('GetAds', err, callback);
-      }
-  },
+    },
+    GetAds: (adsReqArr, callback) => {
+        let ads = [];
 
-  AdClicked: (eventObject, callback) => {
-    let queryByName = {campaign_name: eventObject.campaign_name};
+        try {
 
-    try {
-      
-      decremetValue(queryByName, 'clicks_left', () => {
-        logger.LogEvent(eventObject);
-        callback();
-      });
-      
-    } catch (err) {
-      handleErrors('AdClicked', err, callback);
+            for (let i = 0; i < adsReqArr.length; i++) {
+                ads.push(getAd(adsReqArr[i]));
+            }
+
+            Promise.all(ads).then((data) => {
+                callback(data);
+                for (let i = 0; i < ads.length; i++) {
+                    let query = {
+                        campaign: { campaign_id: ads[i].campaign_id },
+                        ad: { ad_id: ads[i].ad_id }
+                    };
+                    decremetValue(query, 'views_left', () => { });
+                }
+                decrementViews(data);
+            });
+
+        } catch (err) {
+            handleErrors('GetAds', err, callback);
+        }
+    },
+
+    AdClicked: (eventObject, callback) => {
+        let query = { campaign_id: eventObject.campaign_id };
+
+        try {
+
+            decremetValue(query, 'clicks_left', () => {
+                logger.LogEvent(eventObject);
+                callback();
+            });
+
+        } catch (err) {
+            handleErrors('AdClicked', err, callback);
+        }
     }
-  },
-
-  AdViewed: (eventObject, callback) => {
-    let queryByName = {campaign_name: eventObject.campaign_name};
-
-    try {
-      decremetValue(queryByName, 'views_left', () => {
-      logger.LogEvent(eventObject);
-      callback();
-      });   
-      
-    } catch (err) {
-      handleErrors('AdViewed' ,err, callback);
-    }
-  }
 }
 
 // ################### Private Methods ################### //
 
-function getAd(pos){
+function getAd(pos) {
 
-  return new Promise((res, rej) =>{
-  DAL.Get(ADS_COL, {isActive: true, positions: pos}, (data) =>{
-    if(data.length === 0){
-      res({campaign_name: 'no_result'});
-    }else{
-      res(data[Math.floor(Math.random() * (data.length))])
-    }
-      });
+    return new Promise((res, rej) => {
+        DAL.Get(ADS_COL, { isActive: true, positions: pos }, (data) => {
+            if (data.length === 0) {
+                res({ campaign_id: 'no_result' });
+            } else {
+                res(data[Math.floor(Math.random() * (data.length))])
+            }
+        });
     });
 }
 
-function validateCampaign (queryByName) {
-  DAL.Get(CAMPAIGN_COL, queryByName, (data) => {
-    if ((data[0] !== undefined) && ((data[0].views_left <= 0) || (data[0].clicks_left <= 0))) {
-        deactivateCampaign(queryByName, () => {
-      });
-    }
-  });
-}
-
-function deactivateCampaign(query, callback){
-  DAL.Update(CAMPAIGN_COL, query, { $set: {isActive: false}}, (data) => {
-    console.log(query.campaign_name, " is not active anymore");
-    deactivateResources(query.campaign_name);
-    callback();
-  });
-}
-
-function validateResources(){
-  DAL.Get(CAMPAIGN_COL, {isActive: false}, (data) => {
-    data.forEach(campaign => {
-      deactivateResources(campaign.campaign_name);
+function validateCampaign(query, active) {
+    DAL.Update(CAMPAIGN_COL, query, { $set: { isActive: active } }, (data) => {
+        console.log('Campaigns modified: ' + data.modifiedCount);
+        validateResources(query, active);
     });
-  });
 }
 
-function deactivateResources(name){
-  DAL.Update(ADS_COL, {campaign_name: name}, {$set: {isActive: false}}, (data) => {
-    console.log("deactivated " + name + " resources", data);
-  });
-}
-
-function decrementViews(ads){
-  for (let i = 0; i < ads.length; i++) {
-    let query = {
-        campaign:{campaign_name: ads[i].campaign_name},
-        ad: {ad_id: ads[i].ad_id}
-      };
-      decremetValue(query, 'views_left', () =>{});
-  }
+function validateResources(query, active) {
+    DAL.Update(ADS_COL, query, { $set: { isActive: active } }, (data) => {
+        console.log('Resources modified: ' + data.modifiedCount);
+    });
 }
 
 function decremetValue(query, key, callback) {
-  let campaignUpdateQuery = {};
-  let adUpdateQuery = {};
+    let campaignUpdateQuery = {};
+    let adUpdateQuery = {};
 
-  switch (key) {
-    case "views_left":
-      campaignUpdateQuery = {$inc: { views_left: -1} };
-      adUpdateQuery = {$inc: { views: +1} };
+    switch (key) {
+        case "views_left":
+            campaignUpdateQuery = { $inc: { views_left: -1 } };
+            adUpdateQuery = { $inc: { views: +1 } };
 
-      break;
-    case "clicks_left":
-      campaignUpdateQuery = {$inc: { clicks_left: -1} };
-      adUpdateQuery = {$inc: { clicks: +1} };
+            break;
+        case "clicks_left":
+            campaignUpdateQuery = { $inc: { clicks_left: -1 } };
+            adUpdateQuery = { $inc: { clicks: +1 } };
 
-      break;
-      default:
+            break;
+        default:
+            callback();
+
+            break;
+    }
+
+    DAL.Update(CAMPAIGN_COL, query.campaign, campaignUpdateQuery, (data) => {
         callback();
+        DAL.Get(CAMPAIGN_COL, query, (data) => {
+            if ((data[0] !== undefined) && ((data[0].views_left <= 0) || (data[0].clicks_left <= 0))) {
+                validateCampaign(query, false);
+            }
+        });
+    });
 
-      break;
-  }
-
-  DAL.Update(CAMPAIGN_COL, query.campaign, campaignUpdateQuery, (data) => {
-    callback();
-    validateCampaign(query); 
-  });
-
-  DAL.Update(ADS_COL, query.ad, adUpdateQuery, (data) => {
-    callback();
-  });
+    DAL.Update(ADS_COL, query.ad, adUpdateQuery, (data) => {
+        callback();
+    });
 }
 
 function handleErrors(src, err, callback) {
-  logger.LogError(err.name, src ,err.message, err);
-  console.log(err);
-  callback(err.name);
+    logger.LogError(err.id, src, err.message, err);
+    console.log(err);
+    callback(err.id);
 }
