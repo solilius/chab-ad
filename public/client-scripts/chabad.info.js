83665f7570e20b8de6916aef9084b5e032442b41
Sol Elan <solipod8@gmail.com>
2019-06-05 12:55:47 +0300

    edit the test (report click)

---
+++ public/client-scripts/chabad.info.js
@@ -0,0 +1,55 @@
+var IDS = ["chabadinfo.com-example_1"];
+var MOBILE_IDS = ["chabadinfo.com-example_1-m"];
+var currIds = ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ? MOBILE_IDS : IDS;
+var ads_array = [];
+
+requestAds();
+
+function requestAds(){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+                insertAds(JSON.parse(xhr.response));
+        }
+    };
+    xhr.open('GET', '/bannersHandler/?banners=' + idsToString(currIds));
+    xhr.send();
+}
+
+function insertAds(ads){
+    ads_array = ads;
+    for(var i=0; i < ads.length; i++){
+        var banner = document.getElementById(currIds[i]);
+        banner.src = ads[i].url;
+        banner.name = i;
+    }
+}
+
+function idsToString(arr){
+    var str = "";
+    for(var i=0; i < arr.length; i++){
+        str += arr[i] + ',';
+    }
+    return str.substring(0, str.length-1);
+}
+
+function onAdClicked(event, index){
+    var body = {
+        campaign_id: ads_array[index].campaign_id,
+        ad_id: ads_array[index].ad_id,
+        action: "clicked",
+        message: "banner got clicked",
+        site: location.href,
+        ad_position: event.currentTarget.id,
+        ip: "ip",
+        date: new Date()
+    }
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/bannersHandler/click', true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(body));
+
+    window.open(ads_array[index].onclick, '_blank');
+}
+
+
