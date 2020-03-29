830ec94c81aee02f98df4adf200b05dcb621dbf2
סול אילן <solipod8@gmail.com>
2020-03-29 15:12:56 +0300

    refactor mobile_ids

--- public/client-scripts/chabad.info.js
+++ public/client-scripts/chabad.info.js
@@ -67,75 +67,7 @@ var IDS = [
     "chabad.info-midcol_under_buisindex_2",
 ];
 
-var MOBILE_IDS =[
-    "chabad.info-top_banner-m",
-    "chabad.info-golden_left-m",
-    "chabad.info-single-m",
-    "chabad.info-news_top-m",
-    "chabad.info-post_1-m",
-    "chabad.info-post_2-m",
-    "chabad.info-post_3-m",
-    "chabad.info-post_4-m",
-    "chabad.info-post_5-m",
-    "chabad.info-post_6-m",
-    "chabad.info-post_7-m",
-    "chabad.info-post_8-m",
-    "chabad.info-post_9-m",
-    "chabad.info-post_10-m",
-    "chabad.info-post_11-m",
-    "chabad.info-post_12-m",
-    "chabad.info-post_13-m",
-    "chabad.info-post_14-m",
-    "chabad.info-post_15-m",
-    "chabad.info-post_16-m",
-    "chabad.info-post_17-m",
-    "chabad.info-post_18-m",
-    "chabad.info-post_19-m",
-    "chabad.info-post_20-m",
-    "chabad.info-post_21-m",
-    "chabad.info-post_22-m",
-    "chabad.info-post_23-m",
-    "chabad.info-post_24-m",
-    "chabad.info-post_25-m",
-    "chabad.info-post_26-m",
-    "chabad.info-post_27-m",
-    "chabad.info-post_28-m",
-    "chabad.info-post_29-m",
-    "chabad.info-post_30-m",
-    "chabad.info-post_31-m",
-    "chabad.info-post_32-m",
-    "chabad.info-post_33-m",
-    "chabad.info-post_34-m",
-    "chabad.info-post_35-m",
-    "chabad.info-post_36-m",
-    "chabad.info-sidebar_magazine_1-m",
-    "chabad.info-sidebar_magazine_2-m",
-    "chabad.info-sidebar_magazine_3-m",
-    "chabad.info-sidebar_above_video_1-m",
-    "chabad.info-sidebar_above_video_2-m",
-    "chabad.info-sidebar_above_video_3-m",
-    "chabad.info-sidebar_under_hotnews_1-m",
-    "chabad.info-sidebar_under_hotnews_2-m",
-    "chabad.info-sidebar_under_hotnews_3-m",
-    "chabad.info-sidebar_under_editor_1-m",
-    "chabad.info-sidebar_under_editor_2-m",
-    "chabad.info-sidebar_under_editor_3-m",
-    "chabad.info-sidebar_under_masia_1-m",
-    "chabad.info-sidebar_under_masia_2-m",
-    "chabad.info-sidebar_under_masia_3-m",
-    "chabad.info-sidebar_under_infomag_1-m",
-    "chabad.info-sidebar_under_infomag_2-m",
-    "chabad.info-sidebar_under_infomag_3-m",
-    "chabad.info-sidebar_under_deot_1-m",
-    "chabad.info-sidebar_under_deot_2-m",
-    "chabad.info-sidebar_under_deot_3-m",
-    "chabad.info-sidebar_test_1-m",
-    "chabad.info-under_mag_widget-m",
-    "chabad.info-midcol_under_buisindex_1-m",
-    "chabad.info-midcol_under_buisindex_2-m"
-];
-
-var currIds = ( /Android|webOS|iPhone|!tablet|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ? MOBILE_IDS : IDS;
+var currIds = ( /Android|webOS|iPhone|!tablet|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ? getMobileIDS(IDS) : IDS;
 var ads_array = [];
 
 requestAds();
@@ -199,4 +131,12 @@ function onAdClicked(event, index){
 
 function elementHasClass(element, className) {
     return (' ' + element.className + ' ').indexOf(' ' + className+ ' ') > -1;
+}
+
+function getMobileIDS(IDs_arr){
+    var mobiles = [];
+    for (let i = 0; i < IDs_arr.length; i++) {
+        mobiles[i] = IDs_arr[i] + "-m";
+    }
+    return mobiles
 }
